6824a75ca50715a318a40fbaa12fde2322ae0614
BoomboxRapsody <seonghyeon802@gmail.com>
2025-10-20 22:24:34 +0900

    Update UseLocalFramework.ps1

--- UseLocalFramework.ps1
+++ UseLocalFramework.ps1
@@ -1,4 +1,4 @@
-# Run this script to use a local copy of osu-framework rather than fetching it from nuget.
+# Run this script to use a local copy of ProjectYomi-framework rather than fetching it from nuget.
 # It expects the osu-framework directory to be at the same level as the osu directory
 #
 # https://github.com/ppy/osu-framework/wiki/Testing-local-framework-checkout-with-other-projects
@@ -12,18 +12,18 @@ dotnet remove $GAME_CSPROJ reference ppy.osu.Framework;
 dotnet remove $ANDROID_PROPS reference ppy.osu.Framework.Android;
 dotnet remove $IOS_PROPS reference ppy.osu.Framework.iOS;
 
-dotnet sln $SLN add ../osu-framework/osu.Framework/osu.Framework.csproj `
-    ../osu-framework/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj `
-    ../osu-framework/osu.Framework.Android/osu.Framework.Android.csproj `
-    ../osu-framework/osu.Framework.iOS/osu.Framework.iOS.csproj;
+dotnet sln $SLN add ../ProjectYomi-framework/osu.Framework/osu.Framework.csproj `
+    ../ProjectYomi-framework/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj `
+    ../ProjectYomi-framework/osu.Framework.Android/osu.Framework.Android.csproj `
+    ../ProjectYomi-framework/osu.Framework.iOS/osu.Framework.iOS.csproj;
 
-dotnet add $GAME_CSPROJ reference ../osu-framework/osu.Framework/osu.Framework.csproj;
-dotnet add $ANDROID_PROPS reference ../osu-framework/osu.Framework.Android/osu.Framework.Android.csproj;
-dotnet add $IOS_PROPS reference ../osu-framework/osu.Framework.iOS/osu.Framework.iOS.csproj;
+dotnet add $GAME_CSPROJ reference ../ProjectYomi-framework/osu.Framework/osu.Framework.csproj;
+dotnet add $ANDROID_PROPS reference ../ProjectYomi-framework/osu.Framework.Android/osu.Framework.Android.csproj;
+dotnet add $IOS_PROPS reference ../ProjectYomi-framework/osu.Framework.iOS/osu.Framework.iOS.csproj;
 
 # workaround for dotnet add not inserting $(MSBuildThisFileDirectory) on props files
-(Get-Content "osu.Android.props") -replace "`"..\\osu-framework", "`"`$(MSBuildThisFileDirectory)..\osu-framework" | Set-Content "osu.Android.props"
-(Get-Content "osu.iOS.props") -replace "`"..\\osu-framework", "`"`$(MSBuildThisFileDirectory)..\osu-framework" | Set-Content "osu.iOS.props"
+(Get-Content "osu.Android.props") -replace "`"..\\ProjectYomi-framework", "`"`$(MSBuildThisFileDirectory)..\ProjectYomi-framework" | Set-Content "osu.Android.props"
+(Get-Content "osu.iOS.props") -replace "`"..\\ProjectYomi-framework", "`"`$(MSBuildThisFileDirectory)..\ProjectYomi-framework" | Set-Content "osu.iOS.props"
 
 # needed because iOS framework nupkg includes a set of properties to work around certain issues during building,
 # and those get ignored when referencing framework via project, threfore we have to manually include it via props reference.
@@ -31,7 +31,7 @@ dotnet add $IOS_PROPS reference ../osu-framework/osu.Framework.iOS/osu.Framework
     Foreach-Object {
         if ($_ -match "</Project>")
         {
-            "  <Import Project=`"`$(MSBuildThisFileDirectory)../osu-framework/osu.Framework.iOS.props`"/>"
+            "  <Import Project=`"`$(MSBuildThisFileDirectory)../ProjectYomi-framework/osu.Framework.iOS.props`"/>"
         }
 
         $_
@@ -40,16 +40,16 @@ dotnet add $IOS_PROPS reference ../osu-framework/osu.Framework.iOS/osu.Framework
 $TMP=New-TemporaryFile
 
 $SLNF=Get-Content "osu.Desktop.slnf" | ConvertFrom-Json
-$SLNF.solution.projects += ("../osu-framework/osu.Framework/osu.Framework.csproj", "../osu-framework/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj")
+$SLNF.solution.projects += ("../ProjectYomi-framework/osu.Framework/osu.Framework.csproj", "../ProjectYomi-framework/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj")
 ConvertTo-Json $SLNF | Out-File $TMP -Encoding UTF8
 Move-Item -Path $TMP -Destination "osu.Desktop.slnf" -Force
 
 $SLNF=Get-Content "osu.Android.slnf" | ConvertFrom-Json
-$SLNF.solution.projects += ("../osu-framework/osu.Framework/osu.Framework.csproj", "../osu-framework/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj", "../osu-framework/osu.Framework.Android/osu.Framework.Android.csproj")
+$SLNF.solution.projects += ("../ProjectYomi-framework/osu.Framework/osu.Framework.csproj", "../ProjectYomi-framework/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj", "../ProjectYomi-framework/osu.Framework.Android/osu.Framework.Android.csproj")
 ConvertTo-Json $SLNF | Out-File $TMP -Encoding UTF8
 Move-Item -Path $TMP -Destination "osu.Android.slnf" -Force
 
 $SLNF=Get-Content "osu.iOS.slnf" | ConvertFrom-Json
-$SLNF.solution.projects += ("../osu-framework/osu.Framework/osu.Framework.csproj", "../osu-framework/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj", "../osu-framework/osu.Framework.iOS/osu.Framework.iOS.csproj")
+$SLNF.solution.projects += ("../ProjectYomi-framework/osu.Framework/osu.Framework.csproj", "../ProjectYomi-framework/osu.Framework.NativeLibs/osu.Framework.NativeLibs.csproj", "../ProjectYomi-framework/osu.Framework.iOS/osu.Framework.iOS.csproj")
 ConvertTo-Json $SLNF | Out-File $TMP -Encoding UTF8
 Move-Item -Path $TMP -Destination "osu.iOS.slnf" -Force
